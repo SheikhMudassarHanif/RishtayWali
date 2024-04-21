6a40f2361e595f336aa4174a557872ec8ad04f01
SheikhMudassarHanif <sheikhmudassar1942003@gmail.com>
2024-04-21 18:21:26 +0500

    check email

--- mvc/app.js
+++ mvc/app.js
@@ -1,5 +1,7 @@
 var express =require("express");
 const indexRouter = require('./routes/index');
+const bodyParser = require('body-parser');
+const formController = require('./routes/signupmanage');
 
 var app=express();
 app.set( "view engine" , "ejs");
@@ -7,10 +9,19 @@ app.set("views", __dirname + "/views");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
 app.use("/",indexRouter);
 app.listen(3000,()=>{console.log('Server is running on port 3000')});
-//for css
+//for css and js files
 app.use(express.static('./views'));
+
+
+
+// Middleware to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// POST route to handle form submission
+app.post('/submitsignup-form', formController.handleFormSubmission);
 
 
 
---
+++ mvc/routes/signupmanage.js
@@ -0,0 +1,107 @@
+// Import necessary modules
+const nodemailer = require('nodemailer');
+
+// Function to handle form submission
+async function handleFormSubmission(req, res) {
+    try {
+        const email = req.body.email;
+        await sendEmail(email);
+        res.send('Email sent successfully!');
+    } catch (error) {
+
+        console.error('Error sending email:', error);
+        res.status(500).send('Error sending email'+email);
+    }
+}
+
+
+
+
+
+
+
+// var transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//       user: 'f219176@cfd.nu.edu.pk',
+//       pass: 'Mudassar@194'
+//     }
+//   });
+  
+//   var mailOptions = {
+//     from: 'f219176@cfd.nu.edu.pk',
+//     to: email,
+//     subject: 'Sending Email using Node.js',
+//     text: 'That was easy!'
+//   };
+  
+//   transporter.sendMail(mailOptions, function(error, info){
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log('Email sent: ' + info.response);
+//     }
+//   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Function to send email using Nodemailer
+function sendEmail(email) {
+    return new Promise((resolve, reject) => {
+        // Create Nodemailer transporter
+        let transporter = nodemailer.createTransport({
+            // Configure your email provider here
+            service: 'gmail',
+            auth: {
+            user: 'f219176@cfd.nu.edu.pk',
+            pass: 'Mudassar@194'
+    }
+        });
+
+        // Email options
+        let mailOptions = {
+            from: 'f219176@cfd.nu.edu.pk',
+            to: email,
+            subject: 'Testing',
+            text: 'Body of your email'
+        };
+
+        // Send email
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error('Error sending email:', error);
+                reject(error);
+            } else {
+                console.log('Email sent:', info.response);
+                resolve();
+            }
+        });
+    });
+}
+
+// Export the function to be used in other files
+module.exports = {
+    handleFormSubmission
+};
