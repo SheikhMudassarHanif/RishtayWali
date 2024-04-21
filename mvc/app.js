5470509f0cc2a944e3988e36bb2a9b356dfd828f
Mustafa Munir <mustafa.munir@live.com>
2024-04-21 13:55:47 +0500

    mvc

---
+++ mvc/app.js
@@ -0,0 +1,10 @@
+var express =require("express");
+const indexRouter = require('./routes/index');
+var app=express();
+app.set( "view engine" , "ejs");
+app.set("views", __dirname + "/views");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/",indexRouter);
+app.listen(3000,function(){console.log('Server is running on port 3000')});
